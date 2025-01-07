30c50f8f2f42de44c3c67dd38c7c9f3d73b9ca44
s Day <dayss1224@gmail.com>
2025-01-07 23:53:04 +0800

    [24] 两两交换链表中的节点

---
+++ hot100/24.swap-nodes-in-pairs.cpp
@@ -0,0 +1,116 @@
+/*
+ * @lc app=leetcode.cn id=24 lang=cpp
+ * @lcpr version=30204
+ *
+ * [24] 两两交换链表中的节点
+ *
+ * https://leetcode.cn/problems/swap-nodes-in-pairs/description/
+ *
+ * algorithms
+ * Medium (73.40%)
+ * Likes:    2334
+ * Dislikes: 0
+ * Total Accepted:    1M
+ * Total Submissions: 1.4M
+ * Testcase Example:  '[1,2,3,4]'
+ *
+ * 给你一个链表，两两交换其中相邻的节点，并返回交换后链表的头节点。你必须在不修改节点内部的值的情况下完成本题（即，只能进行节点交换）。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ * 输入：head = [1,2,3,4]
+ * 输出：[2,1,4,3]
+ *
+ *
+ * 示例 2：
+ *
+ * 输入：head = []
+ * 输出：[]
+ *
+ *
+ * 示例 3：
+ *
+ * 输入：head = [1]
+ * 输出：[1]
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 链表中节点的数目在范围 [0, 100] 内
+ * 0 <= Node.val <= 100
+ *
+ *
+ */
+
+// @lcpr-template-start
+using namespace std;
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <climits>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <queue>
+#include <stack>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+// @lcpr-template-end
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution
+{
+public:
+    ListNode *swapPairs(ListNode *head)
+    {
+        ListNode *Sentail = new ListNode(0, head);
+        ListNode *ahead = Sentail;
+        while(ahead->next != nullptr){
+            ListNode *Before = ahead->next;
+            if(Before->next == nullptr){
+                break;
+            }
+            ListNode *After = Before->next;
+            Before->next = After->next;
+            ahead->next = After;
+            After->next = Before;
+            ahead = Before;
+        }
+        return Sentail->next;
+    }
+};
+// @lc code=end
+
+/*
+// @lcpr case=start
+// [1,2,3,4]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// []\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1]\n
+// @lcpr case=end
+
+ */
