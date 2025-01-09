da8d35afafad30a7f405d322c7da4344d39c38f0
s Day <dayss1224@gmail.com>
2025-01-10 00:05:23 +0800

    [349] 两个数组的交集

---
+++ hot100/349.intersection-of-two-arrays.cpp
@@ -0,0 +1,100 @@
+/*
+ * @lc app=leetcode.cn id=349 lang=cpp
+ * @lcpr version=30204
+ *
+ * [349] 两个数组的交集
+ *
+ * https://leetcode.cn/problems/intersection-of-two-arrays/description/
+ *
+ * algorithms
+ * Easy (75.07%)
+ * Likes:    948
+ * Dislikes: 0
+ * Total Accepted:    639.4K
+ * Total Submissions: 851.1K
+ * Testcase Example:  '[1,2,2,1]\n[2,2]'
+ *
+ * 给定两个数组 nums1 和 nums2 ，返回 它们的 交集 。输出结果中的每个元素一定是 唯一 的。我们可以 不考虑输出结果的顺序 。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 输入：nums1 = [1,2,2,1], nums2 = [2,2]
+ * 输出：[2]
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 输入：nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+ * 输出：[9,4]
+ * 解释：[4,9] 也是可通过的
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 1 <= nums1.length, nums2.length <= 1000
+ * 0 <= nums1[i], nums2[i] <= 1000
+ * 
+ * 
+ */
+
+
+// @lcpr-template-start
+using namespace std;
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <climits>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <queue>
+#include <stack>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+// @lcpr-template-end
+// @lc code=start
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        unordered_set<int> IntSet;
+        unordered_set<int> rtnSet;
+        vector<int> rtnVector;
+        for(int i = 0; i < nums1.size(); i++){
+            IntSet.insert(nums1[i]);
+        }
+        for(int i = 0; i < nums2.size(); i++){
+            if(IntSet.count(nums2[i]) != 0){
+                rtnSet.insert(nums2[i]);
+            }
+        }
+        for(auto i: rtnSet){
+            rtnVector.push_back(i);
+        }
+        return rtnVector;
+
+    }
+};
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [1,2,2,1]\n[2,2]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [4,9,5]\n[9,4,9,8,4]\n
+// @lcpr case=end
+
+ */
+
