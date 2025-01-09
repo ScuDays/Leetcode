e76248eaebbdb741b68309be8df233bddf7be933
s Day <dayss1224@gmail.com>
2025-01-09 23:51:19 +0800

    [242] 有效的字母异位词

---
+++ hot100/242.valid-anagram.cpp
@@ -0,0 +1,98 @@
+/*
+ * @lc app=leetcode.cn id=242 lang=cpp
+ * @lcpr version=30204
+ *
+ * [242] 有效的字母异位词
+ *
+ * https://leetcode.cn/problems/valid-anagram/description/
+ *
+ * algorithms
+ * Easy (67.07%)
+ * Likes:    969
+ * Dislikes: 0
+ * Total Accepted:    888.7K
+ * Total Submissions: 1.3M
+ * Testcase Example:  '"anagram"\n"nagaram"'
+ *
+ * 给定两个字符串 s 和 t ，编写一个函数来判断 t 是否是 s 的 字母异位词。
+ * 
+ * 
+ * 
+ * 示例 1:
+ * 
+ * 输入: s = "anagram", t = "nagaram"
+ * 输出: true
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 输入: s = "rat", t = "car"
+ * 输出: false
+ * 
+ * 
+ * 
+ * 提示:
+ * 
+ * 
+ * 1 <= s.length, t.length <= 5 * 10^4
+ * s 和 t 仅包含小写字母
+ * 
+ * 
+ * 
+ * 
+ * 进阶: 如果输入字符串包含 unicode 字符怎么办？你能否调整你的解法来应对这种情况？
+ * 
+ */
+
+
+// @lcpr-template-start
+using namespace std;
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <climits>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <queue>
+#include <stack>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+// @lcpr-template-end
+// @lc code=start
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size() != t.size()){
+            return false;
+        }
+        int arr[26];
+        for(int i = 0; i < s.size(); i++){
+            arr[s[i] - 'a'] ++;
+            arr[t[i] - 'a'] --;
+        }
+        for(int i = 0; i < 26; i++){
+            if(arr[i] != 0)return false;
+        }
+        return true;
+    }
+};
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// "anagram"\n"nagaram"\n
+// @lcpr case=end
+
+// @lcpr case=start
+// "rat"\n"car"\n
+// @lcpr case=end
+
+ */
+
