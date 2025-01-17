b5e394621d68bfd30770feb14d32834e0b7684e7
s Day <dayss1224@gmail.com>
2025-01-17 13:49:24 +0800

    [151] 反转字符串中的单词

---
+++ hot100/151.reverse-words-in-a-string.cpp
@@ -0,0 +1,134 @@
+/*
+ * @lc app=leetcode.cn id=151 lang=cpp
+ * @lcpr version=30204
+ *
+ * [151] 反转字符串中的单词
+ *
+ * https://leetcode.cn/problems/reverse-words-in-a-string/description/
+ *
+ * algorithms
+ * Medium (57.25%)
+ * Likes:    1250
+ * Dislikes: 0
+ * Total Accepted:    684.2K
+ * Total Submissions: 1.2M
+ * Testcase Example:  '"the sky is blue"'
+ *
+ * 给你一个字符串 s ，请你反转字符串中 单词 的顺序。
+ *
+ * 单词 是由非空格字符组成的字符串。s 中使用至少一个空格将字符串中的 单词 分隔开。
+ *
+ * 返回 单词 顺序颠倒且 单词 之间用单个空格连接的结果字符串。
+ *
+ * 注意：输入字符串 s中可能会存在前导空格、尾随空格或者单词间的多个空格。返回的结果字符串中，单词间应当仅用单个空格分隔，且不包含任何额外的空格。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ * 输入：s = "the sky is blue"
+ * 输出："blue is sky the"
+ *
+ *
+ * 示例 2：
+ *
+ * 输入：s = "  hello world  "
+ * 输出："world hello"
+ * 解释：反转后的字符串中不能存在前导空格和尾随空格。
+ *
+ *
+ * 示例 3：
+ *
+ * 输入：s = "a good   example"
+ * 输出："example good a"
+ * 解释：如果两个单词间有多余的空格，反转后的字符串需要将单词间的空格减少到仅有一个。
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 1 <= s.length <= 10^4
+ * s 包含英文大小写字母、数字和空格 ' '
+ * s 中 至少存在一个 单词
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ * 进阶：如果字符串在你使用的编程语言中是一种可变数据类型，请尝试使用 O(1) 额外空间复杂度的 原地 解法。
+ *
+ */
+
+// @lcpr-template-start
+using namespace std;
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <climits>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <queue>
+#include <stack>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+// @lcpr-template-end
+// @lc code=start
+class Solution
+{
+public:
+    string reverseWords(string s)
+    {
+        vector<string> strVect;
+        for (int i = 0; i < s.size(); i++)
+        {
+            string insert;
+            while (s[i] == ' ' && i < s.size())
+                i++;
+            while (s[i] != ' ' && i < s.size())
+            {
+                insert += s[i];
+                i++;
+            }
+            if (insert.size() != 0)
+            {
+                strVect.push_back(insert);
+            }
+        }
+        string rtn;
+        for (int i = strVect.size() - 1; i >= 0; i--)
+        {
+            rtn += strVect[i];
+            if (i == 0)
+                break;
+            rtn += ' ';
+        }
+        printf("%s\n", rtn.c_str());
+        printf("%d\n", strVect.size());
+        return rtn;
+    }
+};
+// @lc code=end
+
+/*
+// @lcpr case=start
+// "the sky is blue"\n
+// @lcpr case=end
+
+// @lcpr case=start
+// "  hello world  "\n
+// @lcpr case=end
+
+// @lcpr case=start
+// "a good   example"\n
+// @lcpr case=end
+
+ */
