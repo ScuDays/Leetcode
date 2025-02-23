563c2fdf336fda76f5d6f24054a413d9e902a659
s Day <dayss1224@gmail.com>
2025-02-23 21:52:10 +0800

    [199] 二叉树的右视图

---
+++ hot100/199.binary-tree-right-side-view.cpp
@@ -0,0 +1,158 @@
+/*
+ * @lc app=leetcode.cn id=199 lang=cpp
+ * @lcpr version=30204
+ *
+ * [199] 二叉树的右视图
+ *
+ * https://leetcode.cn/problems/binary-tree-right-side-view/description/
+ *
+ * algorithms
+ * Medium (68.45%)
+ * Likes:    1171
+ * Dislikes: 0
+ * Total Accepted:    545.2K
+ * Total Submissions: 787.3K
+ * Testcase Example:  '[1,2,3,null,5,null,4]'
+ *
+ * 给定一个二叉树的 根节点 root，想象自己站在它的右侧，按照从顶部到底部的顺序，返回从右侧所能看到的节点值。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：root = [1,2,3,null,5,null,4]
+ *
+ * 输出：[1,3,4]
+ *
+ * 解释：
+ *
+ *
+ *
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：root = [1,2,3,4,null,null,null,5]
+ *
+ * 输出：[1,3,4,5]
+ *
+ * 解释：
+ *
+ *
+ *
+ *
+ * 示例 3：
+ *
+ *
+ * 输入：root = [1,null,3]
+ *
+ * 输出：[1,3]
+ *
+ *
+ * 示例 4：
+ *
+ *
+ * 输入：root = []
+ *
+ * 输出：[]
+ *
+ *
+ *
+ *
+ * 提示:
+ *
+ *
+ * 二叉树的节点个数的范围是 [0,100]
+ * -100 <= Node.val <= 100 
+ *
+ *
+ */
+
+// @lcpr-template-start
+using namespace std;
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <climits>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <queue>
+#include <stack>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+// @lcpr-template-end
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+// struct TreeNode
+// {
+//     int val;
+//     TreeNode *left;
+//     TreeNode *right;
+//     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+//     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+//     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+// };
+
+class Solution
+{
+public:
+    vector<int> rightSideView(TreeNode *root)
+    {
+        queue<TreeNode *> que;
+        vector<int> result;
+        if(root == nullptr){
+            return result;
+        }
+        que.push(root);
+        while(!que.empty()){
+            int i = que.size();
+            for(int j = 0; j < i; j++){
+                TreeNode *tre = que.front();
+                que.pop();
+                if(j == 0){
+                    result.push_back(tre->val);
+                }
+                if(tre->right != nullptr)que.push(tre->right);
+                if(tre->left != nullptr)que.push(tre->left);
+            }
+        }
+        return result;
+    }
+};
+// @lc code=end
+
+/*
+// @lcpr case=start
+// [1,2,3,null,5,null,4]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1,2,3,4,null,null,null,5]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1,null,3]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// []\n
+// @lcpr case=end
+
+ */
