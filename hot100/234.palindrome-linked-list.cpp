9a8ccfde4e49d56992c196ba4c3d1d4ab8078eeb
s Day <dayss1224@gmail.com>
2025-03-03 21:15:59 +0800

    [234] 回文链表

---
+++ hot100/234.palindrome-linked-list.cpp
@@ -0,0 +1,108 @@
+/*
+ * @lc app=leetcode.cn id=234 lang=cpp
+ * @lcpr version=30204
+ *
+ * [234] 回文链表
+ *
+ * https://leetcode.cn/problems/palindrome-linked-list/description/
+ *
+ * algorithms
+ * Easy (55.65%)
+ * Likes:    2024
+ * Dislikes: 0
+ * Total Accepted:    898.9K
+ * Total Submissions: 1.6M
+ * Testcase Example:  '[1,2,2,1]'
+ *
+ * 给你一个单链表的头节点 head ，请你判断该链表是否为回文链表。如果是，返回 true ；否则，返回 false 。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 输入：head = [1,2,2,1]
+ * 输出：true
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 输入：head = [1,2]
+ * 输出：false
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 链表中节点数目在范围[1, 10^5] 内
+ * 0 <= Node.val <= 9
+ * 
+ * 
+ * 
+ * 
+ * 进阶：你能否用 O(n) 时间复杂度和 O(1) 空间复杂度解决此题？
+ * 
+ */
+
+
+// @lcpr-template-start
+using namespace std;
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <climits>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <queue>
+#include <stack>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+// @lcpr-template-end
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        vector<int> vec;
+        while(head != nullptr){
+            vec.push_back(head->val);
+            head = head->next;
+        }
+
+      
+        for(int i = 0, j = vec.size() - 1; i <= j; i++,j--){
+            if(vec[i] != vec[j])return false;
+        }
+        return true;
+    }
+};
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [1,2,2,1]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1,2]\n
+// @lcpr case=end
+
+ */
+
