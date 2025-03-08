7ea334ceb0487170f3d5b9bf8e7ae4320a9edf59
s Day <dayss1224@gmail.com>
2025-03-09 00:22:00 +0800

    [21] 合并两个有序链表

---
+++ hot100/21.merge-two-sorted-lists.cpp
@@ -0,0 +1,129 @@
+/*
+ * @lc app=leetcode.cn id=21 lang=cpp
+ * @lcpr version=30204
+ *
+ * [21] 合并两个有序链表
+ *
+ * https://leetcode.cn/problems/merge-two-sorted-lists/description/
+ *
+ * algorithms
+ * Easy (67.24%)
+ * Likes:    3707
+ * Dislikes: 0
+ * Total Accepted:    2M
+ * Total Submissions: 2.9M
+ * Testcase Example:  '[1,2,4]\n[1,3,4]'
+ *
+ * 将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。 
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 输入：l1 = [1,2,4], l2 = [1,3,4]
+ * 输出：[1,1,2,3,4,4]
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 输入：l1 = [], l2 = []
+ * 输出：[]
+ * 
+ * 
+ * 示例 3：
+ * 
+ * 输入：l1 = [], l2 = [0]
+ * 输出：[0]
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 两个链表的节点数目范围是 [0, 50]
+ * -100 <= Node.val <= 100
+ * l1 和 l2 均按 非递减顺序 排列
+ * 
+ * 
+ */
+
+
+// @lcpr-template-start
+using namespace std;
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <climits>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <queue>
+#include <stack>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+// @lcpr-template-end
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+    public:
+    
+        ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+            ListNode* Sentinel = new ListNode();
+            ListNode* Current = Sentinel;
+            recursion(Current, list1, list2);
+            return Sentinel->next;
+        }
+        void recursion(ListNode* Current, ListNode* list1, ListNode* list2) {
+            if (list1 == nullptr) {
+                Current->next = list2;
+                return;
+            }
+            if (list2 == nullptr) {
+                Current->next = list1;
+                return;
+            }
+            if (list1->val > list2->val) {
+                Current->next = list2;
+                list2 = list2->next;
+                Current = Current->next;
+            } else {
+                Current->next = list1;
+                list1 = list1->next;
+                Current = Current->next;
+            }
+            recursion(Current, list1, list2);
+        }
+    };
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [1,2,4]\n[1,3,4]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// []\n[]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// []\n[0]\n
+// @lcpr case=end
+
+ */
+
