cad6d841a02064908a8b97d71ff35a272f9e70b4
ScuDays <dayss1224@gmail.com>
2025-08-03 20:28:26 +0800

    [215] 数组中的第K个最大元素

---
+++ hot100/215.kth-largest-element-in-an-array.cpp
@@ -0,0 +1,97 @@
+/*
+ * @lc app=leetcode.cn id=215 lang=cpp
+ * @lcpr version=30204
+ *
+ * [215] 数组中的第K个最大元素
+ *
+ * https://leetcode.cn/problems/kth-largest-element-in-an-array/description/
+ *
+ * algorithms
+ * Medium (60.77%)
+ * Likes:    2788
+ * Dislikes: 0
+ * Total Accepted:    1.4M
+ * Total Submissions: 2.3M
+ * Testcase Example:  '[3,2,1,5,6,4]\n2'
+ *
+ * 给定整数数组 nums 和整数 k，请返回数组中第 k 个最大的元素。
+ * 
+ * 请注意，你需要找的是数组排序后的第 k 个最大的元素，而不是第 k 个不同的元素。
+ * 
+ * 你必须设计并实现时间复杂度为 O(n) 的算法解决此问题。
+ * 
+ * 
+ * 
+ * 示例 1:
+ * 
+ * 输入: [3,2,1,5,6,4], k = 2
+ * 输出: 5
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 输入: [3,2,3,1,2,4,5,5,6], k = 4
+ * 输出: 4
+ * 
+ * 
+ * 
+ * 提示： 
+ * 
+ * 
+ * 1 <= k <= nums.length <= 10^5
+ * -10^4 <= nums[i] <= 10^4
+ * 
+ * 
+ */
+
+
+// @lcpr-template-start
+using namespace std;
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <climits>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <queue>
+#include <stack>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+// @lcpr-template-end
+// @lc code=start
+
+// 用优先级队列 也就是堆。
+// 很简单，但是要记住堆的stl的用法。
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int> pq;
+        for(int i = 0; i < nums.size(); i++){
+            pq.push(nums[i]);
+        }
+        for(int i = 0; i < k - 1; i++){
+            pq.pop();
+        }
+        return pq.top();
+    }
+};
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// 2\n
+// @lcpr case=end
+
+// @lcpr case=start
+// 4\n
+// @lcpr case=end
+
+ */
+
