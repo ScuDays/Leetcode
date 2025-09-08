9b4926aa95abbe9b60b96151f20b8762b8abb4de
ScuDays <dayss1224@gmail.com>
2025-09-08 23:55:52 +0800

    [49] 字母异位词分组

---
+++ hot100/49.group-anagrams.cpp
@@ -0,0 +1,120 @@
+/*
+ * @lc app=leetcode.cn id=49 lang=cpp
+ * @lcpr version=30204
+ *
+ * [49] 字母异位词分组
+ *
+ * https://leetcode.cn/problems/group-anagrams/description/
+ *
+ * algorithms
+ * Medium (70.00%)
+ * Likes:    2395
+ * Dislikes: 0
+ * Total Accepted:    1.2M
+ * Total Submissions: 1.8M
+ * Testcase Example:  '["eat","tea","tan","ate","nat","bat"]'
+ *
+ * 给你一个字符串数组，请你将 字母异位词 组合在一起。可以按任意顺序返回结果列表。
+ * 
+ * 
+ * 
+ * 示例 1:
+ * 
+ * 
+ * 输入: strs = ["eat", "tea", "tan", "ate", "nat", "bat"]
+ * 
+ * 输出: [["bat"],["nat","tan"],["ate","eat","tea"]]
+ * 
+ * 解释：
+ * 
+ * 
+ * 在 strs 中没有字符串可以通过重新排列来形成 "bat"。
+ * 字符串 "nat" 和 "tan" 是字母异位词，因为它们可以重新排列以形成彼此。
+ * 字符串 "ate" ，"eat" 和 "tea" 是字母异位词，因为它们可以重新排列以形成彼此。
+ * 
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 
+ * 输入: strs = [""]
+ * 
+ * 输出: [[""]]
+ * 
+ * 
+ * 示例 3:
+ * 
+ * 
+ * 输入: strs = ["a"]
+ * 
+ * 输出: [["a"]]
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 1 <= strs.length <= 10^4
+ * 0 <= strs[i].length <= 100
+ * strs[i] 仅包含小写字母
+ * 
+ * 
+ */
+
+
+// @lcpr-template-start
+using namespace std;
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <climits>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <queue>
+#include <stack>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+// @lcpr-template-end
+// @lc code=start
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        std::unordered_map<string, vector<string>> rtmap;
+        for(auto it = strs.begin(); it != strs.end(); it ++){
+            string tmp = *it;
+            std::sort(tmp.begin(), tmp.end());
+            rtmap[tmp].push_back(*it);
+        }
+        
+        vector<vector<string>> rt;
+        for(auto it = rtmap.begin(); it != rtmap.end(); it ++){
+            rt.push_back(it->second);
+        }
+        return rt;
+    }
+};
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// ["eat", "tea", "tan", "ate", "nat", "bat"]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [""]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// ["a"]\n
+// @lcpr case=end
+
+ */
+
